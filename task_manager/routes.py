f81134f15700cdcbe0ad21f0fdc65f1e2db4ec57
Queendarlin Nnamani <queendarlinnnamani@gmail.com>
2024-06-04 12:14:46 +0100

    Final touches

--- task_manager/routes.py
+++ task_manager/routes.py
@@ -125,9 +125,7 @@ def login_page():
     form = LoginForm()
     if form.validate_on_submit():
         attempted_user = User.query.filter_by(username=form.username.data).first()
-        if attempted_user and attempted_user.check_password_correction(
-                attempted_password=form.password.data
-        ):
+        if attempted_user and attempted_user.check_password_correction(attempted_password=form.password.data):
             login_user(attempted_user)
             flash(f'Successfully logged in as: {attempted_user.username}', category='success')
             return redirect(url_for('tasks_page'))
