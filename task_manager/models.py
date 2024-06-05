1fd6d45731f04204f5afb724573f160a9833d0a8
Queendarlin Nnamani <queendarlinnnamani@gmail.com>
2024-06-05 15:50:58 +0100

    Added Demo to homepage

--- task_manager/models.py
+++ task_manager/models.py
@@ -57,8 +57,7 @@ class User(db.Model, UserMixin):
             Args:
                 plain_text_password (str): The plain text password.
         """
-        self.password_hash =\
-            bcrypt.generate_password_hash(plain_text_password).decode('utf-8')
+        self.password_hash = bcrypt.generate_password_hash(plain_text_password).decode('utf-8')
 
     def check_password_correction(self, attempted_password):
         """
@@ -70,8 +69,7 @@ class User(db.Model, UserMixin):
             Returns:
                 bool: True if the password matches, False otherwise.
         """
-        return bcrypt.check_password_hash(self.password_hash,
-                                          attempted_password)
+        return bcrypt.check_password_hash(self.password_hash, attempted_password)
 
 
 class Task(db.Model):
